ad22e3bf1f75763f1b1dbe96d76b0c8b07dd379a
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2015-08-21 10:57:02 +0900

    init commit

---
+++ tests/test_basic.py
@@ -0,0 +1,123 @@
+# coding:utf-8
+
+import pytest
+from dictree import Dictree
+
+
+def instanciate():
+    e = Dictree()
+    e[1, 2, 3, 4] = 100
+    e[1, 2, 3, 5] = 200
+    e[1, 2, 3, Dictree.WILDCARD] = 300
+    e[1, 2, 3] = 10
+    e[1, 2, 4] = 20
+    e[1, Dictree.WILDCARD] = 100
+    e[1, Dictree.WILDCARD, 5] = 400
+    e[1, 10, Dictree.WILDCARD, 6] = 500
+    return e
+
+
+def raises_keyerror(f, key):
+    try:
+        f()
+    except KeyError as e:
+        assert e.args[0] == key
+    else:
+        assert False
+
+
+def test_len():
+    elt = Dictree()
+    elt[1, 2] = 2
+    elt[1, 3] = 3
+    elt[1, 3, 4] = 4
+    elt[1, 3, 5] = 5
+    assert len(elt) == 4
+
+
+def test_contains():
+    elt = Dictree()
+    elt[1, 2] = 2
+    elt[1, Dictree.WILDCARD] = 4
+    assert (1, 2) in elt
+    assert (1, Dictree.WILDCARD) in elt
+    assert (1, 4) not in elt
+    assert (1,) not in elt
+
+
+def test_iter():
+    elt = Dictree()
+    elt[1, 2] = 2
+    elt[1, Dictree.WILDCARD] = 3
+    keys = [k for k in elt]
+    assert (1, 2) in keys
+    assert (1, Dictree.WILDCARD) in keys
+    assert len(keys) == 2
+
+
+def test_get():
+    elt = Dictree()
+    elt[1, 2] = 2
+    elt[1, Dictree.WILDCARD] = 3
+    assert elt[1, 2] == 2
+    assert elt[1, 3] == 3
+
+
+def test_get_keyerror():
+    elt = Dictree()
+    elt[1, 2, 3] = 2
+    elt[1, 2, Dictree.WILDCARD] = 3
+    elt[1, 3] = 4
+
+    raises_keyerror(lambda: elt[(1,)], (1,))
+    raises_keyerror(lambda: elt[(1, 2, 3, 4)], (1, 2, 3, 4))
+    raises_keyerror(lambda: elt[(1, 2, 4, 5)], (1, 2, 4, 5))
+
+
+def test_del():
+    elt = Dictree()
+    elt[1, 2, 3] = 2
+    elt[(1,)] = 3
+    del elt[1, 2, 3]
+    assert len(elt) == 1
+
+    elt = Dictree()
+    elt[1, 2, 3] = 1
+    del elt[1, 2, 3]
+    assert not elt.has_subtree
+
+
+def test_del_keyerror():
+    elt = Dictree()
+    elt[1, 2, 3] = 1
+    elt[1, 2, Dictree.WILDCARD] = 2
+
+    raises_keyerror(lambda: elt.__delitem__((1, 2, 4)), (1, 2, 4))
+    raises_keyerror(lambda: elt.__delitem__((1, 2)), (1, 2))
+
+
+def test_find():
+    elt = Dictree()
+    elt[1, 2] = 1
+    assert elt.find((1, 2, 3)) == (1, (False, False))
+    assert elt.find((1, 2)) == (1, (False, False))
+
+    elt = Dictree()
+    elt[1, Dictree.WILDCARD] = 1
+    elt[1, Dictree.WILDCARD, 2, 3] = 2
+    assert elt.find((1, 2)) == (1, (False, True))
+    assert elt.find((1, 3, 2)) == (1, (False, True))
+
+    elt = Dictree()
+    elt[2, 3] = 0
+    elt[Dictree.WILDCARD, 1, 2] = 1
+    elt[Dictree.WILDCARD, 2, 3] = 2
+    assert elt.find((2, 1, 2)) == (1, (True, False, False))
+
+
+def test_find_keyerror():
+    elt = Dictree()
+    elt[2, 3] = 0
+    elt[Dictree.WILDCARD, 1, 2] = 1
+    elt[Dictree.WILDCARD, 2, 3] = 2
+    raises_keyerror(lambda: elt.find((1, 1)), (1, 1))
---
+++ tests/test_intf.py
@@ -0,0 +1,103 @@
+# coding:utf-8
+
+from dictree import Dictree
+
+
+def instanciate():
+    e = Dictree()
+    e[1, 2, 3, 4] = 100
+    e[1, 2, 3, 5] = 200
+    e[1, 2, 3, Dictree.WILDCARD] = 300
+    e[1, 2, 3] = 10
+    e[1, 2, 4] = 20
+    e[1, Dictree.WILDCARD] = 30
+    return e
+
+
+def test_popitem():
+    e = instanciate()
+    key, item = e.popitem()
+    assert isinstance(item, int)
+    assert len(e) == 5
+
+    for i in range(5):
+        e.popitem()
+
+    assert not e.has_subtree
+
+
+def test_pop():
+    e = instanciate()
+    item = e.pop((1, 2, 3))
+    assert item == 10
+    assert len(e) == 5
+
+
+def test_iterkeys():
+    e = instanciate()
+    if hasattr(e, 'iterkeys'):  # only in python2.x
+        assert len([k for k in e.iterkeys()]) == 6
+
+
+def test_itervalues():
+    e = instanciate()
+    if hasattr(e, 'itervalues'):  # only in python2.x
+        assert sum(i for i in e.itervalues()) == 660
+
+
+def test_iteritems():
+    e = instanciate()
+    if hasattr(e, 'iteritems'):  # only in python2.x
+        assert sum(i for k, i in e.iteritems()) == 660
+
+
+def test_keys():
+    e = instanciate()
+    assert len([k for k in e.keys()]) == 6
+
+
+def test_values():
+    e = instanciate()
+    assert sum(i for i in e.values()) == 660
+
+
+def test_items():
+    e = instanciate()
+    assert sum(i for k, i in e.items()) == 660
+
+
+def test_get():
+    e = instanciate()
+    assert e.get((1, 2, 3, 4)) == 100
+    assert e.get((1, 2, 3, 6)) == 300
+    assert e.get((1, 2, 5), None) is None
+
+
+def test_update():
+    e1 = instanciate()
+    e2 = Dictree()
+    e2[1, 2, 3, 4] = -100
+    e2[1, 2, 3, 6] = -200
+    e2[1, 2, 3] = -300
+
+    e1.update(e2)
+    assert len(e1) == 7
+    assert e1[1, 2, 3, 4] == -100
+    assert e1[1, 2, 3, 6] == -200
+    assert e1[1, 2, 3] == -300
+    assert e1[1, 2, 3, 7] == 300
+
+
+def test_setdefault():
+    e = instanciate()
+    e.setdefault((1, 2, 3, 4), -100)
+    e.setdefault((1, 2, 3, 6), -200)
+    assert e[1, 2, 3, 4] == 100
+    assert e[1, 2, 3, 6] == -200
+
+
+def test_clear():
+    e = instanciate()
+    e.clear()
+    assert not e.has_item
+    assert not e.has_subtree
