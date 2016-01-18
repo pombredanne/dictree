143c6ee5cf519ae48133208f94945ceafb9e8637
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2016-01-18 17:24:56 +0900

    no recursion, remove prefix-match feature

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,16 +5,16 @@ from dictree import Dictree, WILDCARD
 
 
 def instanciate():
-    e = Dictree()
-    e[1, 2, 3, 4] = 100
-    e[1, 2, 3, 5] = 200
-    e[1, 2, 3, WILDCARD] = 300
-    e[1, 2, 3] = 10
-    e[1, 2, 4] = 20
-    e[1, WILDCARD] = 100
-    e[1, WILDCARD, 5] = 400
-    e[1, 10, WILDCARD, 6] = 500
-    return e
+    dt = Dictree()
+    dt[1, 2, 3, 4] = 100
+    dt[1, 2, 3, 5] = 200
+    dt[1, 2, 3, WILDCARD] = 300
+    dt[1, 2, 3] = 10
+    dt[1, 2, 4] = 20
+    dt[1, WILDCARD] = 100
+    dt[1, WILDCARD, 5] = 400
+    dt[1, 10, WILDCARD, 6] = 500
+    return dt
 
 
 def raises_keyerror(f, key):
@@ -27,98 +27,85 @@ def raises_keyerror(f, key):
 
 
 def test_len():
-    elt = Dictree()
-    elt[1, 2] = 2
-    elt[1, 3] = 3
-    elt[1, 3, 4] = 4
-    elt[1, 3, 5] = 5
-    assert len(elt) == 4
+    dt = Dictree()
+    dt[1, 2] = 2
+    dt[1, 3] = 3
+    dt[1, 3, 4] = 4
+    dt[1, 3, 5] = 5
+    assert len(dt) == 4
 
 
 def test_contains():
-    elt = Dictree()
-    elt[1, 2] = 2
-    elt[1, WILDCARD] = 4
-    assert (1, 2) in elt
-    assert (1, WILDCARD) in elt
-    assert (1, 4) not in elt
-    assert (1,) not in elt
+    dt = Dictree()
+    dt[1, 2] = 2
+    dt[1, WILDCARD] = 4
+    assert (1, 2) in dt
+    assert (1, WILDCARD) in dt
+    assert (1, 4) not in dt
+    assert (1,) not in dt
 
 
 def test_iter():
-    elt = Dictree()
-    elt[1, 2] = 2
-    elt[1, WILDCARD] = 3
-    keys = [k for k in elt]
+    dt = Dictree()
+    dt[1, 2] = 2
+    dt[1, WILDCARD] = 3
+    keys = [k for k in dt]
     assert (1, 2) in keys
     assert (1, WILDCARD) in keys
     assert len(keys) == 2
 
 
 def test_get():
-    elt = Dictree()
-    elt[1, 2] = 2
-    elt[1, WILDCARD] = 3
-    assert elt[1, 2] == 2
-    assert elt[1, 3] == 3
+    dt = Dictree()
+    dt[1, 2] = 2
+    dt[1, WILDCARD] = 3
+    assert dt[1, 2] == 2
+    assert dt[1, 3] == 3
 
 
 def test_get_keyerror():
-    elt = Dictree()
-    elt[1, 2, 3] = 2
-    elt[1, 2, WILDCARD] = 3
-    elt[1, 3] = 4
+    dt = Dictree()
+    dt[1, 2, 3] = 2
+    dt[1, 2, WILDCARD] = 3
+    dt[1, 3] = 4
 
-    raises_keyerror(lambda: elt[(1,)], (1,))
-    raises_keyerror(lambda: elt[(1, 2, 3, 4)], (1, 2, 3, 4))
-    raises_keyerror(lambda: elt[(1, 2, 4, 5)], (1, 2, 4, 5))
+    raises_keyerror(lambda: dt[(1,)], (1,))
+    raises_keyerror(lambda: dt[(1, 2, 3, 4)], (1, 2, 3, 4))
+    raises_keyerror(lambda: dt[(1, 2, 4, 5)], (1, 2, 4, 5))
 
 
 def test_del():
-    elt = Dictree()
-    elt[1, 2, 3] = 2
-    elt[(1,)] = 3
-    del elt[1, 2, 3]
-    assert len(elt) == 1
+    dt = Dictree()
+    dt[1, 2, 3] = 2
+    dt[(1,)] = 3
+    del dt[1, 2, 3]
+    assert len(dt) == 1
 
-    elt = Dictree()
-    elt[1, 2, 3] = 1
-    del elt[1, 2, 3]
-    assert len(elt) == 0
+    dt = Dictree()
+    dt[1, 2, 3] = 1
+    del dt[1, 2, 3]
+    assert len(dt) == 0
 
 
 def test_del_keyerror():
-    elt = Dictree()
-    elt[1, 2, 3] = 1
-    elt[1, 2, WILDCARD] = 2
+    dt = Dictree()
+    dt[1, 2, 3] = 1
+    dt[1, 2, WILDCARD] = 2
 
-    raises_keyerror(lambda: elt.__delitem__((1, 2, 4)), (1, 2, 4))
-    raises_keyerror(lambda: elt.__delitem__((1, 2)), (1, 2))
+    raises_keyerror(lambda: dt.__delitem__((1, 2, 4)), (1, 2, 4))
+    raises_keyerror(lambda: dt.__delitem__((1, 2)), (1, 2))
 
 
 def test_find():
-    elt = Dictree()
-    elt[1, 2] = 1
-    assert elt.find((1, 2, 3)) == (1, (False, False))
-    assert elt.find((1, 2)) == (1, (False, False))
-
-    elt = Dictree()
-    elt[1, WILDCARD] = 1
-    elt[1, WILDCARD, 2, 3] = 2
-    assert elt.find((1, 2)) == (1, (False, True))
-    assert elt.find((1, 3, 2)) == (1, (False, True))
-
-    elt = Dictree()
-    elt[2, 3] = 0
-    elt[WILDCARD, 1, 2] = 1
-    elt[WILDCARD, 2, 3] = 2
-    assert elt.find((2, 1, 2)) == (1, (True, False, False))
+    dt = Dictree()
+    dt[1, 2] = 1
+    assert dt.find((1, 2)) == (1, (False, False))
 
 
 def test_find_keyerror():
-    elt = Dictree()
-    elt[2, 3] = 0
-    elt[WILDCARD, 1, 2] = 1
-    elt[WILDCARD, 2, 3] = 2
-    raises_keyerror(lambda: elt.find((1, 1)), (1, 1))
-    raises_keyerror(lambda: elt.find((1, 1, 2, 3), True), (1, 1, 2, 3))
+    dt = Dictree()
+    dt[2, 3] = 0
+    dt[WILDCARD, 1, 2] = 1
+    dt[WILDCARD, 2, 3] = 2
+    raises_keyerror(lambda: dt.find((1, 1)), (1, 1))
+    raises_keyerror(lambda: dt.find((1, 1, 2, 3)), (1, 1, 2, 3))
